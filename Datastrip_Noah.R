9827c40e2a052bfe194182638906a278c343e9fd
Noah <minimops.noah@gmail.com>
2020-11-26 15:07:20 +0100

    added StripnWrite_GWL, file to edit and save gwl data set

--- Datastrip_Noah.R
+++ Datastrip_Noah.R
@@ -1,26 +1,5 @@
 #this is a first stab at the datasets
 library(data.table)
-library(stringr)
-
-#gwl data
-gwl_data <- as.data.table(read.table("Data/GWL_1900-2010.csv", header = TRUE, sep = ";",
-                       na.strings = " "))
-#long to wide:
-gwl_data <- melt(gwl_data, id.vars = c("JAHRMONAT"), measure.vars = patterns("^X"),
-     variable.name = "day", value.name = "gwl")
-#split year and month
-gwl_data[, ":=" (year = substr(JAHRMONAT, 1, 4), 
-                 month = substr(JAHRMONAT, 5, 6))][, JAHRMONAT := NULL]
-#rename and pad day
-gwl_data[, day := str_pad(sub("X", "", day), 2, "left", "0")]
-#fuse to date
-gwl_data[, date := as.Date(paste(year, month, day, sep = "-"))]
-#delete columns
-gwl_data[, ":=" (year = NULL, month = NULL, day = NULL)]
-#reorder cols
-setcolorder(gwl_data, c("date", "gwl"))
-
-
 
 #param dataset
 cli_data <- readRDS("Data/data_reanalysis_20201109.rds")
@@ -37,11 +16,12 @@ cli_data_2k <- subset(cli_data, format(as.Date(date),"%Y") %in%
 #convert to data.table for further analysis
 cli_data_2k_dt <- as.data.table(cli_data_2k)
 
-
+#complare with gwl in that timeframe
+gwl_2000s <- readRDS("Data/gwl_2000s_split.rds")
 #check if all 29 gwl are present in this subset
-nrow(unique(gwl_data[format(as.Date(date),"%Y") %in% seq(2000, 2010), .(gwl)]))
-#this timeframe does include all 29+1 gwl
-table(gwl_data[format(as.Date(date),"%Y") %in% seq(2000, 2010), .(gwl)])
+nrow(unique(gwl_2000s[, .(gwl)]))
+#this timeframe does includes 31 gwl
+table(gwl_2000s[, .(gwl)])
 #and every single one is represented at least 13 times
 
 #there are always 4 measurements per day:
---
+++ StripnWrite_GWL.R
@@ -0,0 +1,33 @@
+### This file prepares the dataset of gwl data and saves the results###
+
+library(data.table)
+library(stringr)
+
+#gwl data
+gwl_data <- as.data.table(read.table("Data/GWL_1900-2010.csv", header = TRUE, sep = ";",
+                                     na.strings = " "))
+#long to wide:
+gwl_data <- melt(gwl_data, id.vars = c("JAHRMONAT"), measure.vars = patterns("^X"),
+                 variable.name = "day", value.name = "gwl")
+#split year and month
+gwl_data[, ":=" (year = substr(JAHRMONAT, 1, 4), 
+                 month = substr(JAHRMONAT, 5, 6))][, JAHRMONAT := NULL]
+#rename and pad day
+gwl_data_split <- copy(gwl_data[, day := str_pad(sub("X", "", day), 2, "left", "0")])
+#fuse to date
+gwl_data[, date := as.Date(paste(year, month, day, sep = "-"))]
+#delete columns
+gwl_data[, ":=" (year = NULL, month = NULL, day = NULL)]
+#reorder cols
+setcolorder(gwl_data, c("date", "gwl"))
+
+#subset timeframe
+gwl_2000s <- gwl_data_split[year %in% seq(2000, 2010), ]
+
+
+#saving datasets to Data folder
+saveRDS(gwl_data_split, "Data/gwl_split.rds")
+saveRDS(gwl_data, "Data/gwl.rds")
+saveRDS(gwl_2000s, "Data/gwl_2000s_split.rds")
+
+
