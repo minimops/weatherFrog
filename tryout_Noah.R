bdb7c457dd8e5849986a688bed35f939caa72be6
Noah <minimops.noah@gmail.com>
2020-11-30 13:03:09 +0100

    plotting geo data

--- tryout_Noah.R
+++ tryout_Noah.R
@@ -23,3 +23,27 @@ fit <- hclust(d, method="ward")
 plot(fit) # display dendogram
 
 
+
+#drawing into map
+# require(rgdal)
+# require(ggplot2)
+# 
+# fn <- file.path(tempdir(), "GBR_adm_gdb.zip", fsep = "\\")
+# download.file("https://gisco-services.ec.europa.eu/distribution/v2/nuts/download/ref-nuts-2010-10m.shp.zip", fn)
+# utils::unzip(fn, exdir = tempdir())
+# shp <- readOGR(dsn = file.path(tempdir(), "GBR_adm1.shp"), stringsAsFactors = F)
+
+library(ggplot2)
+library(sf)
+library(rgeos)
+library(rnaturalearth)
+library(rnaturalearthdata)
+# load data
+world <- ne_countries(scale = "medium", returnclass = "sf")
+# gene world map
+ggplot(data = world) +
+  geom_sf() +
+  labs( x = "Longitude", y = "Latitude") +
+  ggtitle("World map", subtitle = paste0("(", length(unique(world$admin)), " countries)")) +
+  geom_point(data = one_day, aes(x = longitude, y = latitude, color = avg_mslp))
+
