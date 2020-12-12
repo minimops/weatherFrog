ddcba2037676ba6b1be67edd361c499932ede903
minimops <minimops.noah@gmail.com>
2020-12-12 12:53:28 +0100

    compatibility for linux

--- visualization_map.R
+++ visualization_map.R
@@ -24,6 +24,7 @@ names(cli_data_2k_avgDay_mslp) <- as.character(seq(1, 20))
 
 
 library(rnaturalearth)
+library(rnaturalearthdata)
 # load data
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
