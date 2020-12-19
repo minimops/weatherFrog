df1dfe163bd12e602acf2cc447c020fa29758c23
Anne Gritto <Anne.Gritto@campus.lmu.de>
2020-12-19 20:54:27 +0100

    folien ergänzt

--- clusterclara.R
+++ clusterclara.R
@@ -1,10 +1,21 @@
-#### Clara clustern: 320 Dimensionen
+#### Clara clustern: 320 Dimensionen#################################################################################
+## Clara steht für Clustering LARge Applications
+## basiert auf k-medoids Approach, also PAM
+## Methodik: 1. Ziehen einer Stichprobe aus Datensatz, die in k vorgegebene Cluster eingeteilt wird mit k-medoids bzw PAM.
+##              In jedem Cluster gibt es ein repräsenatives Objekt, also k Stück
+##           2. Jede Beobachtung, die nicht in der Stichprobe enthalten ist, wird dem am nächsten liegenden der k repräsentativen 
+##              Objekte zugeteilt.
+##      Das ganze wird 5 Mal wiederholt und der Algorithmus entscheidet sich für die Lösung, bei der die geringste mittlere Distanz
+##      innerhalb der k cluster gemessen wurde.
+## Clara eigent sich für 100-1000 Dimensionen mit bis zu 2000 - 3000 Beobachtungen
 
 ## laden der Pakete
 library(cluster)
 library(ggplot2)
 library(data.table)
 library(factoextra)
+library(ggfortify)
+library(ggmosaic)
 ## daten im wide format einlesen
 data.wide <- readRDS("Data/cli_data_05_avgDay_wide.rds")
 data.complete <- readRDS("Data/cli_data_05.rds")
@@ -19,12 +30,24 @@ fviz_nbclust(as.data.frame(scale(data.wide[, 2:321])), FUNcluster = kmeans,
              diss = dist(scale(data.wide[, 2:321]), method = "euclidean"))
 ## geht bei mir leider nicht, da 0.5.0 geladen ist oder so, kann das mal bitte wer anders ausführen?
 
+### normierung funktioniert noch nicht so
+min <- data.wide[, apply(.SD, 2, min), .SDcols = j]
+max <- data.wide[, apply(.SD, 2, max), .SDcols = j]
+j <- 2:321
+data.normiert <- data.wide[, lapply(.SD, function(x) ((.SD - min[x]) / (max[x] - min[x]))), .SDcols = j]
+min[1]
 
-?clara
-dist.data.scaled <- dist(scale(data.wide[, 2:321]), method = "euclidean")
-clusterclara <- clara(scale(data.wide[, 2:321]), k = 29, metric = "euclidean", 
-                      stand = FALSE, samples = 100)
+data.wide[, lapply(.SD, function(x) (.SD - min[x]) / (max[x] - min[x])), .SDcols = j]
+
+
+
+#### clustern
+set.seed(1289)
+# dist.data.scaled <- dist(scale(data.wide[, 2:321]), method = "euclidean")
+clusterclara <- clara(scale(data.wide[, 2:321]), k = 9, metric = "euclidean", 
+                      stand = FALSE, samples = 1000)
 summary(clusterclara)
+?clara
 clusterclara$clustering
 
 data.wide.cluster <- data.wide[, cluster := clusterclara$clustering]
@@ -35,20 +58,24 @@ plot(clusterclara)
 
 
 
-library(ggmosaic)
 mosaic_cluster_clara <- ggplot(data = data.wide.cluster.gwl) + geom_mosaic(aes(x = product(gwl, cluster), fill = gwl), 
                                                                     na.rm = TRUE) +
   labs(x = " Cluster", y = "GWL")
 
 mosaic_gwl_clara <- ggplot(data = data.wide.cluster.gwl) + geom_mosaic(aes(x = product(cluster, gwl), fill = as.factor(cluster)), 
                                                                 na.rm = TRUE) 
+mosaicplot(table(data.wide.cluster.gwl$gwl, data.wide.cluster.gwl$cluster), color = TRUE, main = "Clustering Large Applications", 
+           ylab = "Cluster", xlab = "GWL", cex.axis = 0.6, las = 2)
+mosaicplot(table(data.wide.cluster.gwl$cluster, data.wide.cluster.gwl$gwl), color = TRUE)
 
-gwl
+?mosaicplot
+
 
 
 dist.data.scaled.complete <- dist(scale(data.completed[, 2:1281]), method = "euclidean")
 clusterclara.complete <- clara(scale(data.completed[, 2:1281]), k = 9, metric = "euclidean", 
-                      stand = FALSE, samples = 100)
+                      stand = FALSE, samples = 1000)
+
 summary(clusterclara.complete)
 clusterclara.complete$clustering
 
@@ -60,7 +87,6 @@ plot(clusterclara)
 
 
 
-library(ggmosaic)
 mosaic_cluster_clara_complete <- ggplot(data = data.wide.cluster.gwl.complete) + geom_mosaic(aes(x = product(gwl, cluster), fill = gwl), 
                                                                            na.rm = TRUE) +
   labs(x = " Cluster", y = "GWL")
--- clustermahalanobis.R
+++ clustermahalanobis.R
@@ -3,103 +3,94 @@
 ## ertsmal einlesen
 data.wide <- readRDS("Data/cli_data_05_avgDay_wide.rds")
 
+### Pakete laden
+library(data.table)
+library(ggplot2)
+library(ggmosaic)
+library(factoextra)
+library(ggfortify)
+library(cluster)
 #### einmal alles mit gescalten Variablen
 data.scaled <- scale(data.wide[, 2:321], scale=TRUE, center=TRUE)
+gwl <- readRDS("Data/gwl.rds")
 ## abchecken obs stimmt
 mean <- colMeans(data.scaled)
+# Lovarianzmatrix erstellen
 variance <- var(data.scaled)
-covariance <- cov(data.scaled)
+# Anzahl der Iterationen
 n <- nrow(data.scaled)
+
 ?mahalanobis
-dist_mahal <- matrix(NA, nrow=n, ncol=n)
-dist_mahal_cov <- matrix(NA, nrow=n, ncol=n)
-for(i in 1:n){
-  dist_mahal[i,] <- mahalanobis(data.scaled, data.scaled[i,], variance)
+
+dist_mahal_scaled <- matrix(NA, nrow=n, ncol=n)
+
+# Mahalanobisdistanz berechnen und in Distanzmatrix abspeichern
+for(i in seq_len(n)){
+  dist_mahal_scaled[i,] <- mahalanobis(data.scaled, data.scaled[i,], variance)
 }
 
-for(i in 1:n){
-  dist_mahal_cov[i,] <- mahalanobis(data.scaled, data.scaled[i,], covariance)
-}
 
-rownames(dist_mahal) <- colnames(dist_mahal) <- rownames(dist_mahal_dist) <- colnames(dist_mahal_dist) <- rn <- 1:1826
-dist_mahal <- as.dist(dist_mahal)
+rownames(dist_mahal_scaled) <- colnames(dist_mahal_scaled) <- rn <- 1:1826
+# dist_mahal_scaled <- as.dist(dist_mahal)
 
+# sollte das eigentlich von alleine machen, aber geht nicht
 ??mahalanobis.dist
+saveRDS(dist_mahal_scaled, "Data/dist_mahal_scaled.rds")
 
-complete_linkage <- hclust(dist_mahal, method="complete")
+# erster Clusterversuch, hierarchisch
+complete_linkage <- hclust(as.dist(dist_mahal_scaled), method="complete")
 summary(complete_linkage)
 plot(complete_linkage)
 fviz_dend(complete_linkage) + ggtitle("Complete Linkage - Dendrogramm")
 complete_linkage$order
 
-
-clust <- hcut(dist_mahal, k = 9, hc_func = "hclust", hc_method = "complete")
+## zweiter Clusterversuch
+clust <- hcut(as.dist(dist_mahal_scaled), k = 9, hc_func = "hclust", hc_method = "complete")
 clust$height
-## mean für center Argument bei mahalanobis
-mean <- sapply(data.wide[, 2:321], mean)
-## kovarianzmatrix für Argument cov in mahalanobis
-covariance <- cov(as.data.frame(data.wide[, 2:321]))
-dim(covariance)
-# cov2cor(covariance)
+plot(clust)
 
 
 ### Fuzzy analysis
-# teilt nicht in ein festes cluster ein, sondern erlaubt acu mehrdeutigkeit
+# teilt nicht in ein festes cluster ein, sondern erlaubt auch mehrdeutigkeit
 # hält mehr detaillierte informationen von den daten
-
+set.seed(1289)
 ?fanny
-clusterfanny <- fanny(as.dist(dist_mahal_cov), k = 9, diss = TRUE, memb.exp = 1)
+clusterfanny <- fanny(as.dist(dist_mahal_scaled), k = 9, diss = TRUE, memb.exp = 1)
 
 data.wide.cluster.fanny <- data.wide[, cluster := clusterfanny$clustering]
 data.wide.cluster.gwl.fanny <- gwl[data.wide.cluster.fanny, on = .(date)]
 fanny.plot <- autoplot(clusterfanny, frame = TRUE, frame.type = "norm")                                                         
 clara.plot
 plot(clusterfanny)
+mosaicplot(table(data.wide.cluster.gwl.fanny$gwl, data.wide.cluster.gwl.fanny$cluster), color = TRUE, main = "Cluster der GWL", 
+           ylab = "Cluster", xlab = "GWL", cex.axis = 0.35)
+mosaicplot(table(data.wide.cluster.gwl.fanny$cluster, data.wide.cluster.gwl.fanny$gwl), color = TRUE)
+# -> funktioniert nicht
 
-
-### k -means
+### k -means clustering 
 # kmeans-Clustering mit allen Kovariablen, 100 zufaellige Startpartitionen
-set.seed(356)
 
-clusterkmeans <- kmeans(x=dist_mahal, centers=9, iter.max=100, nstart=100)
+
+clusterkmeans <- kmeans(as.dist(dist_mahal_scaled), centers=9, iter.max=1000)
 
 data.wide.cluster.kmeans <- data.wide[, cluster := clusterkmeans$cluster]
 data.wide.cluster.gwl.kmeans <- gwl[data.wide.cluster.kmeans, on = .(date)]
 kmeans.plot <- autoplot(clusterkmeans, frame = TRUE, frame.type = "norm")                                                         
 kmeans.plot
-plot(clusterkmeans)
 
 
-
-library(ggmosaic)
 mosaic_cluster_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(gwl, cluster), fill = gwl), 
-                                                                           na.rm = TRUE) +
-  labs(x = " Cluster", y = "GWL")
-
-mosaic_gwl_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(cluster, gwl), fill = as.factor(cluster)), 
+                                                                           na.rm = TRUE) + labs(x = " Cluster", y = "GWL")
+mosaic_gwl_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(cluster, gwl), 
+                                                                                   fill = as.factor(cluster)), 
                                                                        na.rm = TRUE) 
-
-clusterkmeans_cov <- kmeans(x= as.dist(dist_mahal_cov), centers=9, iter.max=100, nstart=100)
-
-data.wide.cluster.kmeans <- data.wide[, cluster := clusterkmeans_cov$cluster]
-data.wide.cluster.gwl.kmeans <- gwl[data.wide.cluster.kmeans, on = .(date)]
-kmeans.plot <- autoplot(clusterkmeans, frame = TRUE, frame.type = "norm")                                                         
-kmeans.plot
-plot(clusterkmeans)
+mosaicplot(table(data.wide.cluster.gwl.kmeans$gwl, data.wide.cluster.gwl.kmeans$cluster), color = TRUE, 
+           main = "Cluster mit k-Means und Mahalanobis", ylab = "Cluster", xlab = "GWL", cex.axis = 0.6, las = 2)
+mosaicplot(table(data.wide.cluster.gwl.kmeans$cluster, data.wide.cluster.gwl.kmeans$gwl), color = TRUE)
 
 
 
-library(ggmosaic)
-mosaic_cluster_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(gwl, cluster), fill = gwl), 
-                                                                                   na.rm = TRUE) +
-  labs(x = " Cluster", y = "GWL")
 
-mosaic_gwl_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(cluster, gwl), fill = as.factor(cluster)), 
-                                                                               na.rm = TRUE) 
-
-
-
-library(data.table)
 
 #### einmal alles ohne Skalierung #################################################################################
 
@@ -166,7 +157,7 @@ plot(clusterkmeans)
 
 
 
-library(ggmosaic)
+
 mosaic_cluster_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(gwl, cluster), fill = gwl), 
                                                                                    na.rm = TRUE) +
   labs(x = " Cluster", y = "GWL")
@@ -183,13 +174,5 @@ kmeans.plot
 plot(clusterkmeans)
 
 
-
-library(ggmosaic)
-mosaic_cluster_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(gwl, cluster), fill = gwl), 
-                                                                                   na.rm = TRUE) +
-  labs(x = " Cluster", y = "GWL")
-
-mosaic_gwl_kmeans <- ggplot(data = data.wide.cluster.gwl.kmeans) + geom_mosaic(aes(x = product(cluster, gwl), fill = as.factor(cluster)), 
-                                                                               na.rm = TRUE) 
 
 
