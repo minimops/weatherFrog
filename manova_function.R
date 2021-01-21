0d9a4601bb78291288ca996edfe0ede2aba76b0e
Katja Gutmair <Katja.Gutmair@campus.lmu.de>
2021-01-21 12:49:37 +0100

    Version mit dollar zur spaltenauswahl

--- manova_function.R
+++ manova_function.R
@@ -8,19 +8,19 @@
 # input variables: 
 # cluster_vector:  A vector of integers (from 1:k) indicating the cluster to which each point is allocated.
 # data: our data table with the extracted variables that we are clustering with
-library(data.table)
-wine_subset <- as.data.table(wine_subset)
-data <- wine_subset
-cluster_vector <- cluster
+#library(data.table)
+#wine_subset <- as.data.table(wine_subset)
+#data <- wine_subset
+#cluster_vector <- cluster
 
 manovaFUN(wine_subset,cluster)
 
 manovaFUN <- function(data,cluster_vector){
 data <- cbind(cluster_vector,data)
-print(is.data.table(data))
+data <- as.data.frame(data)
 print(colnames(data))
 
-model <- manova(as.matrix(data[,-1]) ~ data[,1])
+model <- manova(as.matrix(data[,-1]) ~ data$cluster_vector)
 print(model)
 
 sum_model <- summary(model, test = "Wilks")
@@ -33,7 +33,7 @@ print(aov_model)
 # Boxplots der einzelnen Einflussvariablen aufgeteilt nach Cluster
 
 for ( i in seq_len(ncol(data)) - 1){
-  boxplot(data[, i + 1] ~ data[,1], ylab = colnames(data[i + 1]))
+  boxplot(data[, i + 1] ~ data$cluster_vector, ylab = colnames(data[i + 1]))
   
 }
 }
