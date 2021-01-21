b71e0310c8c6fefcf2fa003cfb7fbad3a4128da3
minimops <minimops.noah@gmail.com>
2021-01-22 00:31:39 +0100

    improve efficiency of filter function

--- clustering/Var_Extraction_Method/filter_funs.R
+++ clustering/Var_Extraction_Method/filter_funs.R
@@ -1,5 +1,9 @@
 #trial of a filtering function
 
+library(data.table)
+library(KneeArrower)
+library(checkmate)
+
 #returns the max and min rowid in that order of the given column in the dataset
 getMinMax <- function(data, param) {
   assertDataTable(data)
@@ -11,54 +15,87 @@ getMinMax <- function(data, param) {
   c(maxRow, minRow)
 }
 
+
+#creates clusterinfo (density based with fixed starting points)
 filterDay <- function(data, param) {
   #TODO assertions
   
+  day <- "2006-12-24"
   
-  oneDay <- readRDS("Data/cli_data_2k_avgDay.rds")[date %in% as.Date(day), ][, date := NULL]
+  oneDay <- readRDS("Data/cli_data_2k_avgDay.rds")[date %in% as.Date(day),
+                                  ][, date := NULL]
   
-  drawDay(oneDay, whichFill = "avg_mslp", discrete = FALSE, showGuide = FALSE)
+  #drawDay(oneDay, whichFill = "avg_mslp", discrete = FALSE, showGuide = FALSE)
+  
   
   sc_oneDay <- as.data.table(scale(oneDay))
+  sc_oneDay <- sc_oneDay[, avg_geopot := NULL]
+  #TODO weights
+  #sc_oneDay[, ":=" (latitude = 1.5 * latitude, longitude = 1.5 * longitude)]
   
-  kNNdistplot(oneDay, k = 10)
+  #get denity threshold
+  eps <- findCutoff(seq(1, nrow(sc_oneDay)),
+                  kNNdist(sc_oneDay, k = 10)
+                  , method = "curvature")$y
+
+  #TODO something wrong here
+  startingPoints <- getMinMax(sc_oneDay, "avg_mslp")
+  runIndex <- 0
+  distDT <- copy(sc_oneDay)[, clT := 0]
   
-  y <- kNNdist(sc_oneDay, k = 10)
-  x <- seq(from = 1, to = nrow(sc_oneDay), by = 1)
-  (a <- findCutoff(x,y, method = "curvature")$y)
-  (b <- findCutoff(x,y, method = "first")$y)
-  
-  eps <- 1.5
-  
-  maxpoint <- getMinMax(sc_oneDay, "avg_geopot")[[2]]
-  
-  distList <- c()
-  for (mp in seq_len(nrow(sc_oneDay))) {
-    distList <- c(distList, euclDist(sc_oneDay[maxpoint, ], sc_oneDay[mp, ]))
-  }
-  Nbs <- as.data.table(distList)[distList < eps, clT := 1][is.na(clT), clT := 0]
-  for (i in seq(1,3)) {
-    new_eps <- eps - i * 0.5
-    for (nbs in which(Nbs$clT == 1)) {
-      for (mp in seq_len(nrow(sc_oneDay))) {
-        #x <- c(x, euclDist(sc_oneDay[nbs, ], sc_oneDay[mp, ]))
-        if(euclDist(sc_oneDay[nbs, ], sc_oneDay[mp, ]) < new_eps) Nbs$clT[mp] <- 1
-      }
+  for (cNum in startingPoints) {
+    runIndex <- runIndex + 1
+    
+    distDT[, paste0("distList", runIndex) := euclDist(sc_oneDay, cNum)]
+
+    distDT[get(paste0("distList", runIndex)) < eps, clT := runIndex]
+    
+    for (i in seq(1, 2)) {
+      new_eps <- eps - i * eps/3
+      #new_eps <- eps / (i + 1)
+      
+      #points in the cluster
+      dts <- copy(distDT)[clT == runIndex, ][, 
+                      paste0("distList", runIndex) := NULL][, clT := NULL]
+      #TODO could be made more efficient, dont calc already calculated distances
+      #matrix of the points and the distances to all other points
+      logixMatrix <- apply(dts, MARGIN = 1, FUN = function(x) euclDist(sc_oneDay, 
+                    which(duplicated(rbind(x, as.matrix(sc_oneDay))))) < new_eps)
+
+      changeIndecies <- unique(which(logixMatrix, arr.ind = TRUE)[,1])
+      
+      lapply(changeIndecies, FUN = function(x) {
+          switch (as.character(distDT[x, clT]),
+          "0" = set(distDT, as.integer(x), "clT", runIndex),
+          runIndex = "",
+          if(distDT[x, get(paste0("distList", runIndex))] < 
+             distDT[x, get(paste0("distList", as.character(distDT[x, clT])))]){
+            set(distDT, as.integer(x), "clT", runIndex)
+          }
+          )})
+      
+      # for (nbs in which(Nbs$clT == runIndex)) {
+      #   for (mp in seq_len(nrow(sc_oneDay))) {
+      #     if(euclDist(sc_oneDay[nbs, ], sc_oneDay[mp, ]) < new_eps) Nbs$clT[mp] <- runIndex
+      #   }
+      # }
     }
   }
-
   
   
-  maxClust <- data.table(copy(oneDay), clT = Nbs$clT)
+  maxClust <- data.table(copy(oneDay), clT = distDT$clT)
   
   drawDay(maxClust, "clT", showGuide = F)
   
-  drawDay(oneDay, whichFill = "avg_geopot", discrete = FALSE, showGuide = FALSE)
+  drawDay(oneDay, whichFill = "avg_mslp", discrete = FALSE, showGuide = FALSE)
 }
 
 
-euclDist <- function(Point1, Point2) {
+#returns a vector of euclidean distances of all points in data of row number 
+#pointNr of data
+euclDist <- function(data, pointNr) {
   #TODO assertions
+  useMat <- as.matrix(data)
+  apply(useMat, MARGIN = 1, FUN = function(x) dist(rbind(useMat[pointNr, ], x)))
   
-  dist(rbind(Point1, Point2), method = "euclidean")
 }
