8dd42ea4888d7924962debd67c201bb67c8f3637
Noah <minimops.noah@gmail.com>
2021-02-04 18:18:54 +0100

    saving trial

--- histogrid_params.R
+++ histogrid_params.R
@@ -8,18 +8,23 @@ source("clustering/Var_Extraction_Method/f_extr_funs.R")
 extr_30 <- extrapolate(seq(1971, 2000))
 extr_30_gwl <- as.data.frame(attachGwl(extr_30))
 
-for (param in names(extr_30)[-1]) {
+for (param in names(extr_30)[2:3]) {
  
-  png(filename = paste0("Verteilung pro GWL.png"))
+  # png(filename = paste0(param, "Verteilung_pro_GWL.png"), )
+  # 
+  # ggplot(extr_30_gwl, aes_string(x = param)) +
+  #   ggtitle(paste("Verteilung", param, "pro GWL")) +
+  #   geom_histogram(aes(y=..density..), bins = 30) +
+  #   facet_wrap(~ gwl)
+  # 
+  # dev.off()
   
-  ggplot(extr_30_gwl, aes_string(x = param)) +
-    ggtitle(paste("Verteilung", param, "pro GWL")) +
-    geom_histogram(aes(y=..density..), bins = 30) +
-    facet_wrap(~ gwl)
-  
-  dev.off()
-  
-  # ggsave(plot, filename = paste0("Verteilung pro GWL"), path = "documentation/plots",
-  #        device = "pdf", width = 130, height = 100, limitsize = FALSE)
+  # plot <- ggplot(extr_30_gwl, aes_string(x = param)) +
+  #  ggtitle(paste("Verteilung", param, "pro GWL")) +
+  #  geom_histogram(aes(y=..density..), bins = 30) +
+  #  facet_wrap(~ gwl)
+  # 
+  #  ggsave(plot, filename = paste0(param, "Verteilung pro GWL"),
+  #         device = "png", width = 130, height = 100, limitsize = FALSE)
 }
 
