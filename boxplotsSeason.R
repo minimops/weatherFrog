2330ca6700bd7fe6879b8920ca46f83986278043
Anne Gritto <Anne.Gritto@campus.lmu.de>
2021-02-07 18:25:37 +0100

    boxplots für variablen aus extrapolate() aufgeteilt in seasons

--- boxplotsSeason.R
+++ boxplotsSeason.R
@@ -1,4 +1,10 @@
 #### differences in values of extrapolate() for summer and winter season
+library(data.table)
+
+source("clustering/Var_Extraction_Method/f_extr_funs.R")
+source("clustering/ClusterAssesmentHelper.R")
+
+data <- extrapolate(seq(1971, 2000))
 
 datagwl <- attachGwl(copy(data))
 datagwl
@@ -15,6 +21,57 @@ getSeason2 <- function(dates) {
 season <- getSeason2(datagwl$date)
 datagwl <- datagwl[, season := season]
 
+
+
+################# just for saving the plots!!! ####################
+
+getBoxplotSaved <- function(data, gwl = "all", variable = "mean.mslp") {
+  assertDataTable(data)
+  assertSubset(c("season", "gwl", "date"), names(data))
+  assertCharacter(gwl)
+  
+  if (gwl == "all") {
+    gwl <- unique(datagwl$gwl)
+  }
+  
+  index <- which(names(datagwl) == variable)
+  
+  jpeg(paste0("seasons_", variable, ".jpeg"))
+  par(mfrow = c(3, 5))
+  for (i in seq_len(15)) {
+    datagwlTry <- copy(datagwl)[gwl == gwl[i]]
+    # datagwlSummer <- copy(datagwl)[gwl == i & season == "Summer"]
+    boxplot(datagwlTry[[index]] ~ season, 
+            data = datagwlTry, 
+            xlab = paste0(gwl[i], " per Season"),
+            ylab = variable)
+    
+  }
+  dev.off()
+  
+  jpeg(paste0("seasons_", variable, "2.jpeg"))
+  par(mfrow = c(3, 5))
+  for (i in 16:30) {
+    datagwlTry <- copy(datagwl)[gwl == gwl[i]]
+    # datagwlSummer <- copy(datagwl)[gwl == i & season == "Summer"]
+    boxplot(datagwlTry[[index]] ~ season, 
+            data = datagwlTry, 
+            xlab = paste0(gwl[i], " per Season"),
+            ylab = variable)
+    
+  }
+  dev.off()
+  par(mfrow = c(1, 1))
+}
+
+for (var in names(datagwl)[2:49]) {
+  getBoxplotSaved(copy(datagwl), variable = var)
+}
+
+############################## PLOTS SAVED #################################
+
+# this function is to extract certain variables as boxplots per season
+
 getBoxplot <- function(data, gwl = "all", variable = "mean.mslp") {
   assertDataTable(data)
   assertSubset(c("season", "gwl", "date"), names(data))
@@ -25,16 +82,17 @@ getBoxplot <- function(data, gwl = "all", variable = "mean.mslp") {
   }
   
   index <- which(names(datagwl) == variable)
+  
   par(mfrow = c(3, 5))
   
   for (i in seq_along(gwl)) {
     datagwlTry <- copy(datagwl)[gwl == gwl[i]]
-    # datagwlSummer <- copy(datagwl)[gwl == i & season == "Summer"]
     boxplot(datagwlTry[[index]] ~ season, 
             data = datagwlTry, 
             xlab = paste0(gwl[i], " per Season"),
             ylab = variable)
   }
+  
   par(mfrow = c(1, 1))
 }
 
