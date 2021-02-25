bf07e1056eec3a62388334f9aca13f952aaab331
minimops <minimops.noah@gmail.com>
2021-02-25 16:03:14 +0100

    diff day

---
+++ diff_day.R
@@ -0,0 +1,31 @@
+#diff day
+
+#load original dataset
+cli_data <- readRDS("Data/cli_data.rds")
+
+
+cli_data_30 <- cli_data[format(date, "%Y") %in% seq(1971, 2000), ]
+
+#mslp
+cli_30_mslp <- copy(cli_data_30)[, geopotential := NULL]
+wideTime_30_mslp <- longToWide(cli_30_mslp, id = c("date", "time"),
+                          col = c("longitude", "latitude"),
+                          vars = c("mslp"))
+cols <- names(wideTime_30_mslp)[-c(1,2)]
+maxChange_tile_day_mslp <- copy(wideTime_30_mslp)[, (cols) := lapply(.SD, function(x) max(x) - min(x)),
+                                        by = date, .SDcols = cols][, .SD[1], by = date][, time := NULL] 
+Change_day_mslp <- copy(maxChange_tile_day_mslp)[, .(diff = sum(.SD)), by = date, .SDcols = cols]
+names(Change_day_mslp) <- c("date", "diff_mslp")
+saveRDS(Change_day_mslp, "Data/change_day_mslp.rds")
+
+#geopot
+cli_30_geopot <- copy(cli_data_30)[, mslp := NULL]
+wideTime_30_geopot <- longToWide(cli_30_geopot, id = c("date", "time"),
+                               col = c("longitude", "latitude"),
+                               vars = c("geopotential"))
+cols <- names(wideTime_30_geopot)[-c(1,2)]
+maxChange_tile_day_geopotential <- copy(wideTime_30_geopot)[, (cols) := lapply(.SD, function(x) max(x) - min(x)),
+                                                  by = date, .SDcols = cols][, .SD[1], by = date][, time := NULL] 
+Change_day_geopot <- copy(maxChange_tile_day_geopotential)[, .(diff = sum(.SD)), by = date, .SDcols = cols]
+names(Change_day_geopot) <- c("date", "diff_geopot")
+saveRDS(Change_day_geopot, "Data/change_day_geopot.rds")
--- finalExplore.R
+++ finalExplore.R
@@ -1,8 +1,11 @@
 
 library(cluster)
+library(parallelDist)
+library(parallel)
+library(factoextra)
 source("clustering/Var_Extraction_Method/f_extr_funs.R")
 
-#check if diff overall weights equal difff results
+#check if diff overall weights equal diff results
 dat1 <- extrapolate(seq(1971, 1981))
 dat1s <- scale(copy(dat1)[, date := NULL])
 dist_dat1s <- daisy(dat1s, metric = "manhattan")
@@ -15,16 +18,57 @@ res_dat1sw <- pam(dist_dat1sw, 5, diss = TRUE)
 mean(silhouette(x = res_dat1sw$clustering, dist = dist_dat1sw)[, 3])
 
 #well acc seems to be the same
-#so we dont have to regulariz any dist matrizes
+#so we dont have to regularize any dist matricies
+
+#baseline
+datextr <- extrapolate(seq(1971, 2000))
+
+dist0 <- parallelDist(scale(copy(datextr)[, date := NULL]), method = "manhattan",
+                      threads = detectCores() - 2)
+
+clust0 <- pam(dist0, 5, diss = TRUE)
+
+fviz_silhouette(silhouette(clust0$clustering, dist0))
+
+
 
 #new finer id values better?
 
 #split up diff over day values
+#before split
+diffDay_old <- readRDS("Data/change_day.rds")[format(date, "%Y") %in% seq(1971, 2000)]
+
+datcompl2 <- datextr[diffDay_old, ]
+
+dist2 <- parallelDist(scale(copy(datcompl2)[, date := NULL]), method = "manhattan",
+                      threads = detectCores() - 2)
+
+clust2 <- pam(dist2, 5, diss = TRUE)
+
+fviz_silhouette(silhouette(clust2$clustering, dist2))
+
+
+
+#after split
+diffDay <- readRDS("Data/change_day_mslp.rds")[readRDS("Data/change_day_geopot.rds"), ]
+
+
+datcompl <- datextr[diffDay, ]
+
+dist1 <- parallelDist(scale(copy(datcompl)[, date := NULL]), method = "manhattan",
+             threads = detectCores() - 2)
+
+clust1 <- pam(dist1, 5, diss = TRUE)
+
+fviz_silhouette(silhouette(clust1$clustering, dist1))
 
 #which diff over day calc is better?
 
 #try different weights
 
+#ungewichtet
+#annes gewichte
+#trial
 
 #run final version
 #create final plots
