d9bcef79a40b36ef35e874c67dfb32623bb92f57
Anne Gritto <Anne.Gritto@campus.lmu.de>
2021-03-20 20:54:11 +0100

    weitere Veränderungen

--- mosaicf.R
+++ mosaicf.R
@@ -40,3 +40,52 @@ ggsave("documentation/plots/fplots/mosaicYaxis.png", mosaicYaxis,
        device = "png", width = 7, height = 4)
 
 
+
+## nach Saison
+datafinal <- readRDS("finalDATA/f_data.rds")
+datafinalID <- copy(datafinal)[, cluster := clustID]
+datafinalIDgwl <- attachGwl(copy(datafinalID))
+dataSummer <- separateBySeason(copy(datafinalIDgwl))
+dataWinter <- separateBySeason(copy(datafinalIDgwl), "Winter")
+
+
+mosaicSummer <- ggplot(data = dataSummer) +
+  geom_mosaic(aes(x = product(cluster, gwl), fill = cluster), offset = 0.005) +
+  theme_classic() +
+  ggtitle("Mosaikplot für Cluster ~ GWL im Sommer") +
+  labs(x = "GWL") +
+  scale_fill_brewer(palette = "Set1") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  guides(fill=guide_legend(title = "Cluster", reverse = TRUE)) +
+  theme(axis.ticks.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.line.y = element_blank())
+
+ggsave("bericht/assets/mosaicSummer.png", mosaicSummer,
+       device = "png", width = 9, height = 5)
+
+
+mosaicWinter <- ggplot(data = dataWinter) +
+  geom_mosaic(aes(x = product(cluster, gwl), fill = cluster), offset = 0.005) +
+  theme_classic() +
+  ggtitle("Mosaikplot für Cluster ~ GWL im Winter") +
+  labs(x = "GWL") +
+  scale_fill_brewer(palette = "Set1") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  guides(fill=guide_legend(title = "Cluster", reverse = TRUE)) +
+  theme(axis.ticks.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.line.y = element_blank())
+
+ggsave("bericht/assets/mosaicWinter.png", mosaicWinter,
+       device = "png", width = 9, height = 5)
+
+mosaic(copy(dataSummer), dataSummer$cluster)
+mosaic(copy(dataWinter), dataWinter$cluster)
+
+
+
+
+
