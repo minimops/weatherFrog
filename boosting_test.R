2f62219d816fd652fbb28ccc3c5a27ca42b1a2ec
minimops <minimops.noah@gmail.com>
2021-03-21 12:31:03 +0100

    cluster boosting attempt

---
+++ boosting_test.R
@@ -0,0 +1,22 @@
+#Boosting Test
+
+source("clusterBoosting.R")
+f_data <- readRDS("finalDATA/f_data.rds")
+
+sampling <- c(3, 5)
+
+system.time({
+test <- clusterBoosting(0.1, 2, f_data, sampling, start.weights = rep(0.1, 48))
+saveRDS(test, "Data/boosting/test.rds")
+})
+
+run1 <- clusterBoosting(0.1, 100, f_data, sampling, start.weights = rep(0.1, 48))
+saveRDS(run1, "Data/boosting/run1.rds")
+
+
+run2 <- clusterBoosting(0.1, 100, f_data, sampling, start.weights = rep(0.05, 48))
+saveRDS(run2, "Data/boosting/run2.rds")
+
+
+run3 <- clusterBoosting(0.05, 100, f_data, sampling, start.weights = rep(0.1, 48))
+saveRDS(run3, "Data/boosting/run3.rds")
